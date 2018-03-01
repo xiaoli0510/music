7f3de47d49ad8ebee5509c2412755225e82f20ba
xiaoxiao <253451518@qq.com>
2018-03-01 11:52:08 +0800

    two-bug

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     <MyHeader></MyHeader>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import MyHeader from '../src/components/myHeader/myHeader'
+export default {
+  name: 'App',
+  components:{
+    MyHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
---
+++ src/components/userCenter/userCenter.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="user-center">
+     user-center
+ </div>
+  
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
+
